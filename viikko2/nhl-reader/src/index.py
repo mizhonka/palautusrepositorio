db91120cc2e8dd8ef39f876bb99a6d00dbd38456
Miranda Honkanen <miranda.honkanen@helsinki.fi>
2023-11-07 14:50:41 +0200

    2Teht11

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,6 +1,9 @@
 import requests
 from player import Player
 
+def sort_by_points(player):
+    return player.points
+
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players"
     response = requests.get(url).json()
@@ -10,6 +13,8 @@ def main():
     for player_dict in response:
         player = Player(player_dict)
         players.append(player)
+
+    players.sort(key=sort_by_points)
 
     print("Players from FIN:")
 
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -5,6 +5,7 @@ class Player:
         self.team = dict['team']
         self.goals=dict['goals']
         self.assists=dict['assists']
+        self.points=self.goals+self.assists
 
     def __str__(self):
-        return f"{self.name}: team {self.team}, goals {self.goals}, assists {self.assists}"
+        return f"{self.name:20} {self.team} {self.goals:3}  + {self.assists:3}  = {self.points:3}"
